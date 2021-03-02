079f3eabae7225437fecfdd0910059ae9a2e3d7f
Md. Soliman Ali <65532591+Md-Soliman-Ali@users.noreply.github.com>
2021-03-02 12:10:59 +0600

    Add files via upload

---
+++ Concatenating Two String/concatenating two string.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+
+    char str1[100], str2[100];
+    int i,length=0,j;
+    printf("input two string or char: \n");
+    gets(str1);
+    gets(str2);
+
+    for(i=0; str1[i]!='\0';i++)
+    {
+
+    }
+
+    for(j=0; str2[j]!='\0';j++,i++)
+    {
+      str1[i]=str2[j];
+    }
+
+    str1[i]='\0';
+    printf("concatenating two string: ");
+    puts(str1);
+
+    return 0;
+}
